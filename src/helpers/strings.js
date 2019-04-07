cb278650574dc130c28c935b78fbacd02768cc3b
Schalk Neethling <sneethling@mozilla.com>
2019-04-07 19:42:11 +0200

    Initial commit

---
+++ src/helpers/strings.js
@@ -0,0 +1,26 @@
+module.exports = {
+    queryString: [
+        '#codeforgood',
+        '#civictech',
+        '#codebridge',
+        '#nonprofit',
+        '#opensource'
+    ],
+    queryStringSubQuery: [
+        '',
+        '',
+        ''
+    ],
+    resultType: [
+        'mixed'
+    ],
+    responseString: [
+        'Hi @screenName thanks for the follow! !CR',
+        '@screenName thanks for following! !CR',
+        'Hey @screenName thanks for the follow! !CR',
+        'Thanks for following @screenName! !CR',
+        'Hey @screenName, thanks for following! !CR',
+        'Awesome @screenName, thanks for following! !CR',
+        'Thanks for the follow @screenName! !CR'
+    ]
+};
